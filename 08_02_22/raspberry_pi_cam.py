e4f44eb56c4b568d9f70d018e38905946bebc91d
TruthgamiPC <truthgamilord@hotmail.com>
2022-02-13 15:24:12 +0000

    Small work on UI + actual camera code

--- 08_02_22/raspberry_pi_cam.py
+++ 08_02_22/raspberry_pi_cam.py
@@ -1,14 +1,52 @@
 from picamera import PiCamera
 from time import sleep
 
+from tkinter import *
+from tkinter import ttk
+import tkinter.font as tkFont
+
+win = Tk()
+win.title("Epic Moment?")
+win.geometry("480x320")
+win.configure(background="gray")
+
+# Setting up the buttons
+button_4 = ttk.Button(win, text="Take Photo4",width=13).place(x=350,y=130)
+button_3 = ttk.Button(win, text="Take Photo3",width=13).place(x=350,y=90)
+button_2 = ttk.Button(win, text="Take Photo2",width=13).place(x=350,y=50)
+button_1 = ttk.Button(win, text="Take Photo1",width=13)
+
+#button_1.grid(ipady=20,ipadx=20)
+button_1.place(x=350,y=10)
+
+def exitProgram():
+    print("Exit button pressed")
+    win.quit()
+
+
+win.mainloop()
+
+
+'''        
+def photoTake(camera):
+    camera.resolution = (800, 600)
+    camera.framerate = 15
+    
+    camera.start_preview()
+    sleep(5)
+    camera.capture('/home/pi/Desktop/Honours Code - 1/images/image1.jpg')
+    camera.stop_preview()
+
+
 camera = PiCamera()
+photoTake(camera)
 
 camera.start_preview()
 for i in range(5):
     sleep(5)
     camera.capture('/home/pi/Desktop/image%s.jpg' % i)
 camera.stop_preview()
-
+'''
 # Source: https://projects.raspberrypi.org/en/projects/getting-started-with-picamera/2
 
 # Terminal command for taking a photo with custom size
