89ef09e21023922f18bca2ec2e1ecd7ede4b5d20
TruthgamiPC <43143845+TruthgamiPC@users.noreply.github.com>
2022-03-05 19:31:32 +0000

    Update to work done on PC version - key focus on discoveries with google

--- app/services/vision_basic.py
+++ app/services/vision_basic.py
@@ -1,6 +1,16 @@
 import os
 import io
 
+import argparse
+from enum import Enum
+
+class FeatureType(Enum):
+    PAGE = 1
+    BLOCK = 2
+    PARA = 3
+    WORD = 4
+    SYMBOL = 5
+
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'turnkey-wording-338516-45d2d1f4d5ca.json'
 
 # Imports the Google Cloud client library
@@ -10,7 +20,7 @@ from google.cloud import vision
 client = vision.ImageAnnotatorClient()
 
 # The name of the image file to annotate
-file_name = os.path.abspath('../images/image.jpg')
+file_name = os.path.abspath('out2.jpg')
 
 # Loads the image into memory
 with io.open(file_name, 'rb') as image_file:
@@ -20,7 +30,8 @@ image = vision.Image(content=content)
 
 # Performs label detection on the image file
 #response = client.label_detection(image=image)
-reponse_text = client.text_detection(image=image)
+reponse_text = client.document_text_detection(image=image)
+document = reponse_text.full_text_annotation
 #labels = response.label_annotations
 
 #print('Labels:')
@@ -30,8 +41,34 @@ reponse_text = client.text_detection(image=image)
 print('Text:\n')
 text_data = []
 
+
+
+for page in document.pages:
+    for block in page.blocks:
+        for paragraph in block.paragraphs:
+            for word in paragraph.words:
+                illegal_word = 0
+                length = 0
+                print("---------------------------")
+                for symbol in word.symbols:
+                    length += 1
+                    if not (any(c.isalpha() for c in symbol.text)):
+                        illegal_word = 1
+                        #print(symbol.text)
+
+                if illegal_word != 1:
+                    if length => 1:
+                        if not ()
+                        for symbol in word.symbols:
+                            print(symbol.text)
+
+
+
+
+'''
 for r in reponse_text.text_annotations:
     d = { 'text': r.description}
 
     text_data.append(d)
     print(d)
+'''
